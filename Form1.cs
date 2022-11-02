51862da1dcba2c2623d36c35a911a08d869b8b3a
“Mazlum <mazlum09kaya@gmail.com>
2022-11-02 16:50:11 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekrar3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // [1] 1-100 arasında  5 ve 7'ye bölünebilen sayılar
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i=1 ; i <= 100 ; i++)
+            {
+                if (i % 5 == 0 && i % 7 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+
+        // [2] while - 0-10'a kadar listbox'a ekleme 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i <= 10)
+            {
+                listBox2.Items.Add(i);
+                i++;
+            }
+        }
+
+        // [3] while - 0-10 arası sayılar toplamı
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = 0, sum = 0;
+            while (i < 11)
+            {
+                sum += i;
+                i++;
+            }
+            listBox3.Items.Add(sum);
+        }
+
+        // [4] do while - 0-10'a kadar listbox'a ekleme
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            do
+            {
+                listBox4.Items.Add(i);
+                i++;
+            }
+            while (i <= 10);
+        }
+
+        // [5] Aranan ismi bulma
+        private void button5_Click(object sender, EventArgs e)
+        {
+            String aranan = textBox1.Text;
+            for(int i=0; i< listBox5.Items.Count; i++)
+            {
+                if (aranan == listBox5.Items[i].ToString())
+                {
+                    MessageBox.Show(listBox5.Items[i] + " değeri bulundu");
+                }
+            }
+        }
+
+        // [6] 3 nolu indeksteki değeri getir
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int[] a = { 1, 5, 10, 20, 40, 80, 100 };
+            label1.Text = a[3].ToString();
+        }
+
+        // [7] for ile diziyi listbox'a yazdır
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int[] a = { 1, 5, 10, 20, 40, 80, 100 };
+            for(int i=0; i<a.Length; i++)
+            {
+                listBox6.Items.Add(a[i].ToString());
+            }
+        }
+
+        // [8] dizinin toplamı ve ortalamsı
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int[] a = { 1, 5, 10, 20, 40, 80, 100 };
+            double sum = 0 , avg = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            avg = sum / a.Length;
+            label2.Text = sum.ToString();
+            label3.Text = avg.ToString();
+        }
+
+        // [9] girilen sayıdan küçük değerleri verir
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int[] a = { 1, 5, 10, 20, 40, 80, 100 };
+            int sayi = Convert.ToInt32(textBox2.Text);
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(sayi > a[i])
+                {
+                    listBox7.Items.Add(a[i].ToString());
+                }
+            }
+        }
+
+        // [10] foreach ile diziyi yazdırma
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int[] liste1 = { 1, 5, 10, 20, 40, 80, 100 };
+            foreach(int m in liste1)
+            {
+                listBox8.Items.Add(m.ToString());
+            }
+        }
+
+        // [11] sınırlı sayıdaki liste elemanlarını sıralama
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int[] liste = new int[3];
+            liste[0] = 1;
+            liste[1] = 2;
+            liste[2] = 3;
+
+            //for(int i = 0; i < liste.Length; i++)
+            //{
+            //    listBox9.Items.Add(liste[i]);
+            //}
+
+            foreach(int i in liste)
+            {
+                listBox9.Items.Add(i.ToString());
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("tekrar3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tekrar3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("d75c8ec9-9277-4009-b32c-39620548426d")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
